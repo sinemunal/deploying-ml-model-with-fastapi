16553c4217b2caaafa957a7cacfa6b556cc5de95
Sinem <sinemunal199@gmail.com>
2023-05-29 21:57:16 +0200

    Implement model train methods

---
+++ src/model.py
@@ -0,0 +1,59 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
+
+RF_PARAM_GRID = {
+    "n_estimators": [100, 200],
+    "max_depth": [3, None],
+    "random_state": [42],
+    "class_weight": ["balanced"],
+}
+
+
+def train_model(X_train, y_train, is_optimized=False):
+    """
+    Trains a machine learning model and returns it.
+
+    Inputs
+    ------
+    X_train : np.array
+        Training data.
+    y_train : np.array
+        Labels.
+    is_optimized: boolean
+        When true GridSearchCV is applied for hyperparameter tuning.
+    Returns
+    -------
+    model
+        Trained machine learning model.
+    """
+
+    if is_optimized is False:
+        model = RandomForestClassifier()
+        model.fit(X_train, y_train)
+    else:
+        model_cv = GridSearchCV(
+            RandomForestClassifier(),
+            param_grid=RF_PARAM_GRID,
+            cv=StratifiedKFold(n_splits=3)
+        )
+        model_cv.fit(X_train, y_train)
+        model = model_cv.best_estimator_
+
+    return model
+
+
+def inference(model, X):
+    """ Run model inferences and return the predictions.
+
+    Inputs
+    ------
+    model : RandomForestClassifier
+        Trained machine learning model.
+    X : np.array
+        Data used for prediction.
+    Returns
+    -------
+    preds : np.array
+        Predictions from the model.
+    """
+    return model.predict(X)
---
+++ tests/src/test_model.py
@@ -0,0 +1,42 @@
+from unittest.mock import patch
+
+import pytest
+import numpy as np
+import sklearn
+
+from src.model import train_model, inference
+
+
+@pytest.fixture
+def X_train():
+    return np.array([
+        [1., 1., 0. ],
+        [1., 1., 0.],
+        [1., 1., 0.],
+        [2., 0., 1.]
+    ])
+
+
+@pytest.fixture
+def y_train():
+    return np.array([1, 1, 1, 0])
+
+
+def test_train_model_trains_random_forest_classifier(X_train, y_train):
+    trained_model = train_model(X_train, y_train)
+
+    assert type(trained_model) == sklearn.ensemble._forest.RandomForestClassifier
+
+
+def test_train_model_does_gridsearchcv_when_optimized_is_enables(X_train, y_train):
+    trained_model = train_model(X_train, y_train, is_optimized=True)
+
+    assert trained_model.get_params()['max_depth'] == 3
+
+
+def test_inference_return_predictions_for_binary_classification(X_train, y_train):
+    trained_model = train_model(X_train, y_train)
+    preds = inference(trained_model, X_train)
+
+    assert type(preds) == np.ndarray
+    np.testing.assert_array_equal(np.unique(preds), [0, 1])
