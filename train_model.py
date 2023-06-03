5bd9e4e635dcb07644b47e6dda38d862d3aee749
Sinem <sinemunal199@gmail.com>
2023-06-03 09:49:37 +0200

    Remove redundant checks

--- train_model.py
+++ train_model.py
@@ -54,22 +54,3 @@ test.to_csv("data/test.csv", index=False)
 trained_model = train_model(X_train, y_train, is_optimized=True)
 
 save_model(trained_model, "rf_model.pickle")
-
-print("pricess data from loaded enc.")
-test2 = pd.read_csv("data/test.csv")
-encoder2 = load_model("encoder.pickle")
-lb2 = load_model("label_binarizer.pickle")
-X_test2, y_test2, _, _ = process_data(
-    test2,
-    categorical_features=cat_features,
-    label=target,
-    training=False,
-    encoder=encoder2,
-    lb=lb2,
-)
-
-print("do assertions: ")
-test.index = test2.index
-pd.testing.assert_frame_equal(test2, test)
-np.testing.assert_array_equal(y_test2, y_test)
-np.testing.assert_array_equal(X_test2, X_test)
