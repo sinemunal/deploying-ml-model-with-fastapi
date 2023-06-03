6d90107722d76cb95ab6874a651efc7e1a86d298
Sinem <sinemunal199@gmail.com>
2023-06-03 14:06:58 +0200

    Refactor model_performance

--- model_performance.py
+++ model_performance.py
@@ -5,9 +5,31 @@ from src.data_processing import process_data
 from src.model import inference
 from src.model_helpers import load_model, compute_model_metrics_on_slices
 
+
+def _calculate_inference(test_df, target_name, cat_features):
+    # Load trained model, encoder and binarizer
+    trained_model = load_model("rf_model.pickle")
+    encoder = load_model("encoder.pickle")
+    lb = load_model("label_binarizer.pickle")
+    # Proces the test data with the process_data function.
+    X_test, y_test, _, _ = process_data(
+        test_df,
+        categorical_features=cat_features,
+        label=target_name,
+        training=False,
+        encoder=encoder,
+        lb=lb,
+    )
+
+    # Get predictions
+    y_preds = inference(trained_model, X_test)
+
+    return y_test, y_preds
+
+
 # Settings related to data
 feature_name = "sex"
-target = "salary"  # target column that we classify
+target_name = "salary"  # target column that we classify
 cat_features = [
     "workclass",
     "education",
@@ -22,23 +44,8 @@ cat_features = [
 # Load census data for test
 test = pd.read_csv("data/test.csv")
 
-# Load trained model, encoder and binarizer
-trained_model = load_model("rf_model.pickle")
-encoder = load_model("encoder.pickle")
-lb = load_model("label_binarizer.pickle")
 
-
-# Proces the test data with the process_data function.
-X_test, y_test, _, _ = process_data(
-    test,
-    categorical_features=cat_features,
-    label=target,
-    training=False,
-    encoder=encoder,
-    lb=lb,
-)
-# Get predictions
-y_preds = inference(trained_model, X_test)
+y_test, y_preds = _calculate_inference(test, target_name, cat_features)
 
 # Calculate performance metrics with respect to gender feature
 result_df = compute_model_metrics_on_slices(y_test, y_preds, test[feature_name])
